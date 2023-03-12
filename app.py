0227ffb658c044b54f1e32fd239c575b813f60a9
mrldxes <amiraldorodriguesmdix@gmail.com>
2023-03-12 05:51:48 +0000

    primeiro commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, redirect
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    # Debug True serve para atualizar a página sem parar o servidor
+    app.run(debug=True)
+
